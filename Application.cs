85c56c9bd650cad2938e0ceaa3a8019bad6e461b
Arnab Sen <arnab.sen210@gmail.com>
2020-09-13 15:58:55 +1200

    Initial commit using LoRDeckCodes

---
+++ Application.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoRDeckCodes;
+
+namespace LoRCollectionManager
+{
+    public class Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Test();
+        }
+
+        private Application()
+        {
+
+        }
+
+        private void Test()
+        {
+            var deckCode = "CEBQCAIFF4DQGCJDHBEUWWCZLYCQGBICAMCAKBQAAEAQGCJP";
+            var deck = LoRDeckEncoder.GetDeckFromCode(deckCode);
+
+        }
+    }
+}
